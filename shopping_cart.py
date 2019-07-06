11f871aac38fc63fd1bd621ad2169d031d5ea087
“Wendy <wendysjkim@gmail.com>
2019-07-05 22:25:26 -0400

    finished lab

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,17 +1,41 @@
 class ShoppingCart:
     # write your code here
-    def __init__(self, emp_discount=None):
-      pass
+    def __init__(self, employee_discount=None, items=[]):
+      self.total = 0
+      self.employee_discount = employee_discount
+      self.items = items
+
     def add_item(self, name, price, quantity=1):
-       pass
+      for i in range(quantity):  
+        self.items.append({"name": name, "price":price})
+        self.total += price
+      return self.total
+ 
     def mean_item_price(self):
-       pass
+      prices = sorted([item["price"] for item in self.items], reverse=True)
+      length = len(prices)
+      mean = self.total / length   
+      return mean
 
     def median_item_price(self):
-        pass
+      prices = sorted([item["price"] for item in self.items], reverse=True)
+      length = len(prices)  
+      if length % 2 == 1:
+        median = prices[length//2+1]
+      else:
+        median =  (prices[length//2]+ prices[length//2+1])/2            
+      return median 
 
     def apply_discount(self):
-       pass
+      if self.employee_discount:
+        disc_total =  self.total * (100 - self.employee_discount)/100 
+        return disc_total 
+      else:
+        print("Sorry, there is no discount to apply to your cart :(") 
 
     def void_last_item(self):
-        pass
+      if self.items:
+        removed = self.items.pop()
+      else:
+        return("There are no items in your cart!")
+      self.total -= removed['price']
